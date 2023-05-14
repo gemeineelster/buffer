2074ab66b3a4e463bff8b9ed0787ba8373f3df06
gemeineelster <44088921+gemeineelster@users.noreply.github.com>
2023-05-14 10:35:46 +0200

    Created gps_parser

---
+++ src/gps_parser/gps_parser.h
@@ -0,0 +1,21 @@
+#ifndef GPS_PARSER_H_
+#define GPS_PARSER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GPS_PARSER_H_ */
---
+++ tests/gps_parser/test.cpp
@@ -0,0 +1,23 @@
+#include "CppUTest/TestHarness.h"
+
+
+/*extern "C" {
+    #include "buffer.h"
+}*/
+#include "gps_parser.h"
+#include <string.h>
+
+
+TEST_GROUP(gps_parser_test) {
+
+   void setup()
+   {
+        
+        //printf("In text_4 steht %s", text_4);
+   }
+
+   void teardown()
+   {
+
+   }
+};
